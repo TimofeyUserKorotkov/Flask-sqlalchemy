42d446edcc5293e5a9bf747aaaff9ac98ab71790
TimofeyUserKorotkov <korotkov.timofey.log@yandex.ru>
2024-04-06 01:50:24 +0300

    1

---
+++ main.py
@@ -0,0 +1,59 @@
+from flask import Flask, redirect, render_template
+from data import db_session
+from data.news import News
+from data.users import User
+from flask_login import LoginManager, login_user
+from forms.login import LoginForm
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+def main():
+    db_session.global_init("db/blogs.sqlite")
+    app.run()
+
+    # db_sess = db_session.create_session()
+    
+    # user = User(name="Ridley",
+    #             email="scott_chief@mars.org",
+    #             hashed_password="12345")
+    
+    # db_sess.add(user)
+    
+    # db_sess.commit()
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+if __name__ == '__main__':
+    main()
